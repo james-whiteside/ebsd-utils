3a85f4ec6dda15505fde4c72d3edd42652032774
James Whiteside <117453030+james-whiteside@users.noreply.github.com>
2024-12-21 21:58:06 +0000

    Removed unnecessary transformation from relative to absolute file path.

--- scan_analyse.py
+++ scan_analyse.py
@@ -2,14 +2,13 @@
 
 from datetime import datetime
 from src.utilities.config import Config
-from src.utilities.utilities import get_file_paths, get_directory_path, format_time_interval
+from src.utilities.utilities import get_file_paths, format_time_interval
 from src.scan import Scan
 
 
 def analyse() -> None:
     config = Config()
-    data_dir = get_directory_path(config.project.data_dir)
-    data_paths = get_file_paths(directory_path=data_dir, recursive=True, extension="csv")
+    data_paths = get_file_paths(directory_path=config.project.data_dir, recursive=True, extension="csv")
 
     for data_path in data_paths:
         scan = Scan.from_csv(data_path, config)
@@ -20,8 +19,8 @@ def analyse() -> None:
         if config.analysis.reduce_resolution:
             scan = scan.reduce_resolution(config.resolution.reduction_factor)
 
-        scan.to_csv(get_directory_path(config.project.analysis_dir))
-        scan.to_maps(get_directory_path(config.project.map_dir))
+        scan.to_csv(config.project.analysis_dir)
+        scan.to_maps(config.project.map_dir)
         time_taken = (datetime.now() - start_time).total_seconds()
         print(f"Analysis completed in: {format_time_interval(time_taken)}")
 
